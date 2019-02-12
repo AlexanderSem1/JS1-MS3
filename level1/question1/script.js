a8d33ae8e4dacf48f0d82f4f57a56121220bbee9
Alexander Sem <alexander.sem@icloud.com>
2019-02-12 20:21:13 +0000

    Submitting weekly assignment

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,66 @@
 //Use RegEx to validate form
+
+function validateMyFormInput() {
+    let myInputName = /^[a-zA-Z]+$/;
+    let myInputNumber = /^[0-9]{8,10}$/; 
+    let myInputEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/; 
+    
+    
+    let inputFirstname = document.getElementById("firstname").value;
+    let inputLastName = document.getElementById("lastname").value;
+    let inputPhoneNumber = document.getElementById("phonenumber").value;
+    let inputEmail = document.getElementById("email").value; 
+    
+    (function checkFirstName(){
+        if(inputFirstname.match(myInputName)){
+            document.getElementById("firstnameresult").innerHTML = "First Name is accepted";
+        }else
+        
+        {
+            document.getElementById("firstnameresult").innerHTML = "Please write your first name";
+        }  
+    })(); 
+
+    (function checkLastName(){
+        if(inputLastName.match(myInputName)){
+            document.getElementById("lastnameresult").innerHTML = "Last name is accepted";
+        }
+        else{
+            document.getElementById("lastnameresult").innerHTML = "Please write your last name"; 
+        }
+        
+    })();
+
+
+    (function checkPhoneNumber(){
+        if(inputPhoneNumber.match(myInputNumber)){
+            document.getElementById("phoneresult").innerHTML = "The phone input is correct";
+        }
+        else{
+            document.getElementById("phoneresult").innerHTML = "Phone number is missing";
+        }
+    })();
+
+    (function checkmyEmail(){
+        if(inputEmail.match(myInputEmail)){
+            document.getElementById("emailresult").innerHTML = "The email input is correct ";
+        }
+        else{
+            document.getElementById("emailresult").innerHTML = "Please provide valid email";
+        }
+    })();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,8 @@
 //Console.log out elements in JSON file
+
+var input = '{"id": 12312412312, "name": "Ecuaciones Diferenciales", "url": "/video/math/edo/12312412312", "author": {"data": [{"name_author": "Alejandro Morales", "url": "/author/alejandro-morales", "type": "master"}]}}'
+var jsonObject = JSON.parse(input);
+console.log(jsonObject.id);
+console.log(jsonObject.name);
+console.log(jsonObject.url);
+console.log(jsonObject.author);
